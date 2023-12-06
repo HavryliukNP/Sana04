6069ab75b9070f8133a09ec37caa08eadf79d01b
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2023-12-06 19:33:59 +0200

    Додав 1 завдання

---
+++ Task1/Matrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Matrix
+    {
+        public static int[,] RandomMatrix(int N, int M, int MinValue = -100, int MaxValue = 100)
+        {
+            int[,] array = new int[N, M];
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    array[i, j] = random.Next(MinValue, MaxValue);
+
+            return array;
+        }
+        public static int CountPositive(int[,] array)
+        {
+            int Count = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    if(array[i, j] > 0)
+                        Count++;
+            }
+         return Count;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Task1;
+
+Console.Write("Input N: ");
+var N = int.Parse(Console.ReadLine());
+
+Console.Write("Input M: ");
+var M = int.Parse(Console.ReadLine());
+
+int[,] myArray = Matrix.RandomMatrix(N, M);
+
+Console.WriteLine("Randomly generated array:");
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+        Console.Write($"{myArray[i, j]}\t");
+    Console.WriteLine("\n");
+}
+
+int countPositive = Matrix.CountPositive(myArray);
+Console.WriteLine($"Count positive elements: {countPositive}");
